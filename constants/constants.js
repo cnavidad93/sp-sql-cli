54d9624cb56ca87822958f67cd0556d65e6c7438
Carlos Navidad <carlosnavidadp@lemonade.be>
2022-03-02 23:33:31 +0100

    constants file

---
+++ constants/constants.js
@@ -0,0 +1,40 @@
+export const TYPES = {
+  studierichting: "4d74444a-1c32-47cd-b26b-d186e97981bb",
+};
+
+export const GRADES = {
+  1: "/sam/commons/studyprogrammegroups/b1a7f179-6e7c-497b-be9a-7ca30e4e8c65",
+  2: "/sam/commons/studyprogrammegroups/2ea33583-8819-4137-bb4e-c3efd9e91346",
+};
+
+export const FINALITIES = {
+  A: "/sam/commons/studyprogrammegroups/e79e2ce8-4a7a-4758-b9d4-0e14ad0e6493",
+  D: "/sam/commons/studyprogrammegroups/f9109da7-c299-48e4-8a24-339c994e1af8",
+  "D/A":
+    "/sam/commons/studyprogrammegroups/6e631913-12ef-41ab-895f-ad256adfa2c6",
+};
+
+export const GROUPS = {
+  "MAATSCHAPPIJ EN WELZIJN":
+    "/sam/commons/studyprogrammegroups/98d4afb3-5856-4601-b2b9-ab3d3efb75c6",
+  "VOEDING EN HORECA":
+    "/sam/commons/studyprogrammegroups/70afb99e-edc9-4091-9fcb-e6f677f490d5",
+  STEM: "/sam/commons/studyprogrammegroups/59b5532a-42db-4876-8dc5-b53cea16cec8",
+  "ECONOMIE EN ORGANISATIE":
+    "/sam/commons/studyprogrammegroups/2a409f6c-9766-47ee-99db-4d82d2768cb5",
+  "KUNST EN CREATIE":
+    "/sam/commons/studyprogrammegroups/33215043-bfb1-4900-9cc8-075020165d4c",
+  "LAND- EN TUINBOUW":
+    "/sam/commons/studyprogrammegroups/a026eed5-0af4-4ff3-bd2e-873014f0325d",
+  SPORT:
+    "/sam/commons/studyprogrammegroups/ecc85e50-21da-4c47-90d1-54924704f847",
+  "TAAL EN CULTUUR":
+    "/sam/commons/studyprogrammegroups/cfeab5c1-5c5d-486a-a93c-af32c15e7eab",
+  DOMEINOVERSCHRIJDEND:
+    "/sam/commons/studyprogrammegroups/4cbc9b9b-50e9-45ca-a19c-584bd8caaaa1",
+};
+
+export const CSV_OPTIONS = {
+  SEPARATOR: ";",
+  HEADERS: ["title", "type", "date", "grade", "finality", "group"],
+};
